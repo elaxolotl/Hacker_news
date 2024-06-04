7de95665367521a35b9906ffef0a3ca60c977990
youssefEchadySfaxi <youssefechadysfaxi@gmail.com>
2024-06-04 14:07:59 +0100

    + multiple components

--- src/App.jsx
+++ src/App.jsx
@@ -25,27 +25,33 @@ const list = [
 function getTitle(title) {
   return title;
 }
+
+function List() {
+  return (<ul>
+    {list.map(function (item) {
+      return (
+        <li>{item.title}</li>
+      );
+    })}
+  </ul>)
+}
+
+function Search(){
+  return (
+    <div>
+        <label htmlFor="search">Search: </label>
+        <input id="search" type="text" />
+    </div>
+  );
+};
+
 function App() {
   return (
     <div>
       <h1>Hello {getTitle("React")}</h1>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <Search/>
       <hr />
-      <ul>
-        {list.map(function (item) {
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title} </a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </li>
-          );
-        })}
-      </ul>
+      <List />
     </div>
   )
 }
