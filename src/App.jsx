aba89f1d046c1bc74bc2ed213b5e13b0fe8d0c94
youssefEchadySfaxi <youssefechadysfaxi@gmail.com>
2024-06-04 14:34:05 +0100

    + props

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
 
 function getTitle(title) {
   return title;
 }
 
-const List = () => (
+const List = (props) => (
   <ul>
-    {list.map((item) => (
-        <li key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title} </a>
-          </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-        </li>
-      ))}
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item} />
+    ))}
   </ul>
 )
 
-const Search = () => (
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
-    </div>
+const Item = (list) => (
+  <li key={list.item.objectID}>
+    <span>
+      <a href={list.item.url}>{list.item.title} </a>
+    </span>
+    <span>{list.item.author}</span>
+    <span>{list.item.num_comments}</span>
+    <span>{list.item.points}</span>
+  </li>
 );
 
-const App = () => (
+const Search = () => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" />
+  </div>
+);
+
+const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+  return (
     <div>
-      <h1>Hello {getTitle("React")}</h1>
+      <h1>Hacker stories</h1>
       <Search />
       <hr />
-      <List />
+      <List list={stories} />
     </div>
-  )
+  );
+}
 
 export default App
