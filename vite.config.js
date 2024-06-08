5f606f4e0560b2e350ebd731928246722e38d030
youssefEchadySfaxi <youssefechadysfaxi@gmail.com>
2024-06-08 10:54:52 +0100

    externalized icons

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default {
+  plugins: [
+    react(),
+  ],
   build: {
     rollupOptions: {
-      external: ['react-icons/go']
-    }
-  }
-})
+      external: ['react-icons/fa'],
+    },
+  },
+};
